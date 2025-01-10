8e052afa2b971f1bfc1b6c0ca88ae219a246d0a1
sumukhanidhibs <sumukhanidhi@gmail.com>
2025-01-10 18:28:01 +0530

    ADDED NEW FILES

---
+++ src/main/java/com/aitool/SpringAiTool/SpringAiToolApplication.java
@@ -0,0 +1,13 @@
+package com.aitool.SpringAiTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiToolApplication.class, args);
+	}
+
+}
