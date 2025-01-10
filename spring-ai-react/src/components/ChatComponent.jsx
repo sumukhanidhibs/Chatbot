881a241156113919b383be87925a297edb5851ce
SumukhaNidhi <109661181+sumukhanidhibs@users.noreply.github.com>
2025-01-10 22:48:03 +0530

    recipe and chat component added

--- spring-ai-react/src/components/ChatComponent.jsx
+++ spring-ai-react/src/components/ChatComponent.jsx
@@ -1,7 +1,34 @@
 import React,{useState} from "react";
 
 function ChatComponent(){
-  
+    const [prompt,setPrompt] = useState('');
+    const [chatResponse,setResponse]=useState([]);
+
+    const askAI = async () => {
+        try {
+            const response = await fetch(`http://localhost:8080/ask-ai?prompt=${prompt}`)
+            const data = response.text;
+            setResponse(data);
+        } catch (error) {
+            console.error("Error Generating chat",error)
+        }
+    };
+
+    return (
+        <div>
+            <h2>Talk to AI</h2>
+            <input type="text" 
+            value={prompt} 
+            onChange={(e)=>setPrompt(e.target.value)} 
+            placeholder="Enter a prompt for AI"/>
+
+            <button onClick={askAI}>AskAI</button>
+
+            <div className="output">
+                <p>{chatResponse}</p>
+            </div>
+        </div>
+    )
 }
 
 export default ChatComponent;
--- spring-ai-react/src/components/RecipeGenerator.jsx
+++ spring-ai-react/src/components/RecipeGenerator.jsx
@@ -2,6 +2,44 @@ import React,{useState} from "react";
 
 function RecipeGenerator(){
 
+    const [ingredients,setIngredients] = useState('');
+    const [cusuine,setcuisine]=useState('any');
+    const [recipe,setRecipe] = useState('');
+
+
+    const generateRecipe = async () => {
+        try {
+            const response = await fetch(`http://localhost:8080/recipe-creator?ingredients=${ingredients}&cusuine=${cusuine}`)
+            const data = response.text;
+            setRecipe(data);
+        } catch (error) {
+            console.error("Error Generating recipe",error)
+        }
+    };
+
+    return (
+        <div>
+             <input type="text" 
+            value={ingredients} 
+            onChange={(e)=>setIngredients(e.target.value)} 
+            placeholder="Enter a ingredients for recipe"/>
+
+            <input type="text" 
+            value={cusuine} 
+            onChange={(e)=>setcuisine(e.target.value)} 
+            placeholder="Enter a ingredients for recipe"/>
+
+
+            <button onClick={generateRecipe}>AskAI</button>
+
+            <div className="output">
+                <p>{recipe}</p>
+            </div>
+
+
+        </div>
+    );
+
 }
 
 export default RecipeGenerator;
